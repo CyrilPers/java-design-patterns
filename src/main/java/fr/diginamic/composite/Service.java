02e7559c3a1e07bbfa1b60ff5d15ce1e0426914a
Cyril <cyril.personne@outlook.fr>
2024-07-23 21:20:26 +0200

    correction utiliser add et non set

--- src/main/java/fr/diginamic/composite/Service.java
+++ src/main/java/fr/diginamic/composite/Service.java
@@ -1,11 +1,11 @@
 package fr.diginamic.composite;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Service implements IElement {
     private String nom;
-    private List<Employe> employes;
-    private List<Service> services;
+    private List<IElement> elements;
 
     public Service(String nom) {
         this.nom = nom;
@@ -22,39 +22,25 @@ public class Service implements IElement {
         this.nom = nom;
     }
 
-    public List<Employe> getEmployes() {
-        return employes;
+    public List<IElement> getElements() {
+        return elements;
     }
 
-    public void setEmployes(List<Employe> employes) {
-        this.employes = employes;
+    public void setElements(List<IElement> elements) {
+        this.elements = elements;
     }
 
-    public void setEmployes(Employe employe) {
-        this.employes = List.of(employe);
-    }
-
-    public List<Service> getServices() {
-        return services;
-    }
-
-    public void setServices(List<Service> services) {
-        this.services = services;
-    }
-
-    public void setServices(Service service) {
-        this.services = List.of(service);
+    public void addElement(IElement element) {
+        if (elements == null)
+            elements = new ArrayList<>();
+        elements.add(element);
     }
 
     @Override
     public double calculerSalaire() {
         double salaire = 0;
-        for (Employe employe : employes)
-            salaire += employe.calculerSalaire();
-        if (services != null) {
-            for (Service service : services)
-                salaire += service.calculerSalaire();
-        }
+        for (IElement element : elements)
+            salaire += element.calculerSalaire();
         return salaire;
     }
 }
--- src/test/java/fr/diginamic/composite/CompositeTest.java
+++ src/test/java/fr/diginamic/composite/CompositeTest.java
@@ -10,16 +10,18 @@ public class CompositeTest {
         Service primaryService = new Service("PrimaryService");
         Service secondaryService = new Service("SecondaryService");
         Service tertiaryService = new Service("TertiaryService");
-        primaryService.setServices(secondaryService);
-        secondaryService.setServices(tertiaryService);
+        primaryService.addElement(secondaryService);
+        secondaryService.addElement(tertiaryService);
         Employe employe1 = new Employe("John", "Doe", 1000);
         Employe employe2 = new Employe("Sara", "Miller", 1000);
         Employe employe3 = new Employe("Andrew", "Brown", 1000);
         Employe employe4 = new Employe("Robert", "Davis", 1000);
         Employe employe5 = new Employe("Mc", "Gregor", 10000);
-        primaryService.setEmployes(List.of(employe1, employe2));
-        secondaryService.setEmployes(List.of(employe3, employe4));
-        tertiaryService.setEmployes(List.of(employe5));
+        primaryService.addElement(employe1);
+        primaryService.addElement(employe2);
+        secondaryService.addElement(employe3);
+        secondaryService.addElement(employe4);
+        tertiaryService.addElement(employe5);
 
         assert(primaryService.calculerSalaire() == 14000);
         assert(secondaryService.calculerSalaire() == 12000);
